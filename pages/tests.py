69f313c4973a185ac2d3e3d3c7d507152179745b
renadalkhlafat <renadsalem8888@gmail.com>
2021-11-17 19:38:16 +0200

    feat:(create worked django app)

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+
+class TestSnacks(TestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "_base.html")
+
+    def test_about_us_page_status_code(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_template(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "_base.html")
